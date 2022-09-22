5a661cb02504de0ebc0c36376e37f657b69226e0
anailson ribeiro <arsantos315@gmail.com>
2022-09-22 16:43:02 -0300

    Formulário reativo

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -17,7 +17,7 @@ import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule, FormsModule, MensagemModule
+    HomeRoutingModule, FormsModule, MensagemModule, ReactiveFormsModule
   ],
   exports:[
     HomeComponent
