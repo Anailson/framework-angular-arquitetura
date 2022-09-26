0c69713d7d7147e88b6a08a28b2e7a5e5348712a
anailson ribeiro <arsantos315@gmail.com>
2022-09-26 14:17:59 -0300

    Validações Assíncronas

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,3 +1,4 @@
+import { UsuarioExisteService } from './usuario-existe.service';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -15,18 +16,15 @@ export class NovoUsuarioComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private novoUsuarioService: NovoUsuarioService
+    private novoUsuarioService: NovoUsuarioService,
+    private UsuarioExisteService: UsuarioExisteService
     ) { }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-        email: ['',[
-          Validators.required, Validators.email
-        ]],
-        fullName:['',[
-          Validators.required, Validators.minLength(4)
-        ]],
-        userName: ['',[minusculoValidator]],
+        email: ['',[Validators.required, Validators.email]],
+        fullName:['',[Validators.required, Validators.minLength(4)]],
+        userName: ['',[minusculoValidator],[this.UsuarioExisteService.usuarioJaExite()]],
         password:[''],
     });
   }
