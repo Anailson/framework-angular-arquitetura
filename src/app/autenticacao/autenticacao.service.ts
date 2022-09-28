d3e597a62b91d1bd689ef3c3aa09757042e9cf53
anailson ribeiro <arsantos315@gmail.com>
2022-09-28 14:43:51 -0300

    Serviço de autenticação

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 
-import{HttpClient} from '@angular/common/http'
+import{HttpClient, HttpResponse} from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -9,16 +11,20 @@ import { Observable } from 'rxjs';
 })
 export class AutenticacaoService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private usuarioService: UsuarioService) { }
 
-  autenticar(usuario: string, senha: string): Observable<any>{
-
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>>{
     return this.httpClient.post('http://localhost:3000/user/login',{
         userName: usuario,
         password: senha,
-
-    });
-
+    },
+       {observe: 'response'}
+    ).pipe(
+      tap((res) =>{
+           const authToken = res.headers.get('x-access-token') ?? '';
+           this.usuarioService.salvaToken(authToken);
+      })
+    )
   }
 
 }
